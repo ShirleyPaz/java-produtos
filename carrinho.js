a7d2a44c33a8537895593a7ca3257d034d43ee5f
Shirley Paz <shirley.pazz@gmail.com>
2018-01-12 15:23:56 -0200

    js com variáveis

--- carrinho.js
+++ carrinho.js
@@ -73,7 +73,7 @@ var produtos = [
     //     </td>
     // </tr>
    
-            function listarprodutos() {
+    function listarprodutos() {
     //Gerar o temploate através do array de objetos
     var template = "";
 
@@ -81,36 +81,36 @@ var produtos = [
         template += '<tr class="cart-item row">';
         template += '<td class="cart-item__description col-cl-2 col-tb-4 col-dk-6">'; 
         template += '<i class="fa fa-times-circle"></i>'; 
-        template +='<img class="description__image" src=".\img\camiseta.jpg" alt="camiseta roxa">';
+        template += '<img class="description__image" src="'+produtos[i].imagem[i].src'"alt="'+clientes[i].imagem[i].alt+'">';
         template += '<div class="description-details">'; 
-        template += '<h4 class="description-details__title">Puma Golf Ragian Tech Polo Tee</h4>';
+        template += '<h4 class="description-details__title">'+produtos[i].descricao+'</h4>';
         template += ' <div class="description-details__info">';
-        template += '<p class="description-details-info__label">Color: </p>';
+        template += '<p class="description-details-info__label">Color: </p>'; //como inserir a cor no CSS do ícone
         template += '<i class="fa fa-circle" aria-hidden="true"></i>';
         template += '<p class="description-details-info__label">Size:</p>';
-        template += '<p class="description-details__size-selected">XXL</p>';
+        template += '<p class="description-details__size-selected">'+produtos[i].size+'</p>';
         template += '</div>';
         template += '</div>';
         template += '</td>';
         template += '<td class="item-price col-cl-1 col-tb-2 col-dk-2">';
         template += '<div class="item-price__details">';
         template += '<p class="currency">$</p>';
-        template += '<p class="value">$72</p>';
+        template += '<p class="value">'+produtos[i].price+'</p>';
         template += '</div>';
         template += '</td>';
         template += '<td class="item-quantity col-cl-1 col-tb-2 col-dk-2">';
-        template += '<input type="number" class="item-quantity__value">';
+        template += '<input type="number" class="item-quantity__value">'; //como inserir a qtde no input 
         template += '</td>';
         template += '<td class="item-total col-cl-1 col-tb-2 col-dk-2">';
         template += '<div class="item-total__details">';
         template += '<p class="currency">$</p>';
-        template += '<p class="value">216</p>';
+        template += '<p class="value">'+produtos[i].price*+produtos[i].quantity+'</p>';
         template += '</div>';
         template += '</td>';
         template += '</tr>';
     }
 
-    document.getElementById('products').innerHTML = template;
+    document.getElementById("products").innerHTML = template;
 }
 
 console.log(produtos);
